55a472fe782eb7f74395fad22db56dbe102165ea
Krryxa <krryxq@163.com>
2021-09-07 17:47:14 +0800

    feat: 服务端掉接口

--- vue-ssr/src/entry.client.js
+++ vue-ssr/src/entry.client.js
@@ -5,9 +5,9 @@ const router = app.$router
 const store = app.$store
 
 if (window.__INITIAL_STATE__) {
-  store.replaceState(window.__INITIAL_STATE__)
+  store.replaceState(window.__INITIAL_STATE__) // replaceState 是覆盖 state
 }
 
 router.onReady(() => {
-  app.$mount('#app')      //服务端渲染默认会生成一个id为app的div
+  app.$mount('#app')      // 服务端渲染默认会生成一个 id 为 app 的 div
 })
